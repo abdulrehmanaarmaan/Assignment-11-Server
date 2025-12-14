b791cf54d97011516957d9b98308c08e3f29cf9d
AbdulRehmanAarmaan <abdulrehmanaarmaan@gmail.com>
2025-12-14 11:45:38 +0600

    used MongoDB operators for various APIs

--- index.js
+++ index.js
@@ -295,7 +295,7 @@ async function run() {
                 }
             }
 
-            const result = await requestsCollection.updateOne(query, updatedStatus);
+            const result = await requestsCollection.updateOne(query, updatedStatus)
             res.send(result)
         })
 
@@ -607,7 +607,7 @@ async function run() {
         // console.error(err);
         // res.status(500).send({ success: false, message: err.message });
         // }
-        // })
+        // });
 
 
         app.get('/payment-history', verifyFBToken, verifyHR, async (req, res) => {
